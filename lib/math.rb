db7ffa5493d020cff21eeef0b29a420bdb5571bd
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-21 23:26:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
 def addition(num1, num2)
-
+puts "hi"
+num1 + num2
 end
 
 def subtraction(num1, num2)
-
+puts "hi"
+num1 - num2
 end
 
 def division(num1, num2)
-
+  puts "hi"
+num1/num2
 end
 
 def multiplication(num1, num2)
-
+puts "hi"
+num1*num2
 end
 
 def modulo(num1, num2)
 
 end
 
-def square_root(num)
-
+def square_root(num1, num2) 
 end
