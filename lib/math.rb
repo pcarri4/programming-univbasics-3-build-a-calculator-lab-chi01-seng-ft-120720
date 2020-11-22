03499337e0b84542e70838f784fe95b6d7b698d9
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-22 00:05:55 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+puts "addition"
+num1 + num2
 end
 
 def subtraction(num1, num2)
-
+puts "subtraction"
+num1 - num2
 end
 
 def division(num1, num2)
-
+  puts "division"
+num1/num2
 end
 
 def multiplication(num1, num2)
-
+puts "multiplication"
+num1*num2
 end
 
 def modulo(num1, num2)
-
+puts "modulo"
+num1 % num2
 end
 
 def square_root(num)
-
+puts "square_root"
+Math.sqrt(num)
 end
